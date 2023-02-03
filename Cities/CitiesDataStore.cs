f2abbf5a1fb288faa8539fb71e8d901f763db529
Mohammad AbuSafat <mo7ammad.abusafat@gmail.com>
2023-02-03 19:56:16 +0200

    intilize project add create City Controller

---
+++ Cities/CitiesDataStore.cs
@@ -0,0 +1,36 @@
+﻿using Cities.API.Models;
+
+namespace Cities.API
+{
+    public class CitiesDataStore
+    {
+        public List<CityDto> Cities { get; set; }
+
+        public static CitiesDataStore Current { get; set; } = new CitiesDataStore();
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "Jenin",
+                    Description = "Jenin Description"
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Tulkarm",
+                    Description = "Tulkarm Description"
+                },
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Nablus",
+                    Description = "Nablus Description"
+                }
+            };
+        }
+    }
+}
---
+++ Cities/Controllers/CitiesController.cs
@@ -0,0 +1,22 @@
+﻿using Cities.API;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cities.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{Id}")]
+        public JsonResult GetCity(int Id)
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(city => city.Id == Id));
+        }
+    }
+}
